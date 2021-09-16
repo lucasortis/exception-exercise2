8fb91235ddaea930c389b48f4f3bb1f5ed6ef789
Lucas Ortis <lucasortis01@gmail.com>
2021-09-16 14:48:03 -0300

    implementation of Reservation Class

---
+++ ExerciseExceptions/Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExerciseExceptions.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Reservation: Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
--- ExerciseExceptions/Program.cs
+++ ExerciseExceptions/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-
+using ExerciseExceptions.Entities;
 namespace ExerciseExceptions
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+            Console.WriteLine(reservation);
             
         }
     }
